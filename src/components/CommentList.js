dd5262a686d9ccba08a91b5f7e0b6b2752fe2ad9
Janessa Hanna <jahanna@c02zjqh7md6m.turner.com>
2021-01-20 21:44:06 -0500

    comment list setup

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,21 +1,44 @@
-import '../App.css';
-import React, { useState, useEffect } from 'react';
-import {Api} from '../api/index.js';
+import "../App.css";
+import React, { useState, useEffect } from "react";
+import { Api } from "../api/index.js";
 
 function CommentList() {
+  const [commentsData, setCommentsData] = useState([]);
+
+  Api.get("/getcomments").then(function (result) {
+
+    for (let comment of result) {
+      commentsData.push(comment);
+    }
+
+    setCommentsData(commentsData);
+
+  });
+
+  // display object
+  const commentsFormatted = commentsData.map((comment, i) => {
+
+    let date = Date.parse(comment.created);
+    let day = new Intl.DateTimeFormat('en-GB',{weekday:'long'}).format(date);
+    let time = new Intl.DateTimeFormat('en-US',{timeStyle: "short"}).format(date);
+
+    return (
+      <div key={i} className="comment-container">
+        <p className="comment-post">{comment.message}</p>
+        <p className="comment-poster">{comment.name} on {day} at {time}</p>
+      </div>
+    );
+  });
 
   useEffect(() => {
 
-    console.log(Api);
+
 
   });
 
   return (
     <div className="comment-display">
-        <div className="comment-container">
-          <p className="comment-post">this is my lovely comment</p>
-          <p className="comment-poster">by Margot on Thursday at 4pm</p>
-        </div>
+      {commentsFormatted}
     </div>
   );
 }
